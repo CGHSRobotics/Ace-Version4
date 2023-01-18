928af7b7928e3045cce15e9aa50c0fbad031be22
CGHSRobotics <cghsrobotics@gmail.com>
2023-01-18 09:46:13 -0500

    first commit

---
+++ src/cghs.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "main.h"
+
+// namespace cghs
+namespace cghs {
+/*
+ *  Constants
+ */
+
+// Motor Ports
+const int MOTOR_LEFT_FRONT_PORT = 11;
+const int MOTOR_LEFT_BACK_PORT = 12;
+const int MOTOR_RIGHT_FRONT_PORT = 1;
+const int MOTOR_RIGHT_BACK_PORT = 20;
+const int MOTOR_LAUNCHER_PORT = 17;
+const int MOTOR_ROLLER_PORT = 10;
+const int MOTOR_CONVEYOR_PORT = 15;
+const int MOTOR_INTAKE_PORT = 16;
+
+// Speed Constants
+
+
+
+
+
+}  // namespace cghs
+
